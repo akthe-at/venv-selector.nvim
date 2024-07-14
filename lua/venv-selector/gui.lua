f92cc4bf6c6d4ae4ec79ca960a741988d03e7089
Adam K. <arkelly2@medicine.wisc.edu>
2024-07-13 23:56:12 -0500

    use custom input for picker selection.

--- lua/venv-selector/gui.lua
+++ lua/venv-selector/gui.lua
@@ -1,16 +1,15 @@
 local BasePicker = require("venv-selector.pickers.basepicker")
 local TelescopePicker = require("venv-selector.pickers.telescope")
-local FzfPicker = require("venv-selector.pickers.fzflua")
+local FzfLuaPicker = require("venv-selector.pickers.fzflua")
 
--- local testPicker = require('venv-selector.config').default_settings.options.picker
--- local guiPicker = getGuiPicker()
--- vim.notify(guiPicker)
+-- FIX: can't rely on user_settings.options.picker to be set in time for Venvselect autocmd.
+-- local guiPicker = require("venv-selector.config").user_settings.options.picker
 local guiPicker = "fzf-lua"
 
 if guiPicker == "telescope" then
     M = TelescopePicker:new()
 elseif guiPicker == "fzf-lua" then
-    M = FzfPicker:new()
+    M = FzfLuaPicker:new()
 else
     vim.notify('Invalid picker setting, please select one of "telescope" or "fzf-lua"', vim.log.levels.ERROR)
     vim.notify("The currently selected picker is: " .. guiPicker, vim.log.levels.ERROR)
